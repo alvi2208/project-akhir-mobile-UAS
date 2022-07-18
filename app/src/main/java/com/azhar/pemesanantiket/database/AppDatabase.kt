cffb47058b3e0f7b5dc55368f72a636e87bf8ece
alvi2208 <alviadnanjr@gmail.com>
2022-07-18 09:08:44 +0700

    project-akhir-mobile-UAS

---
+++ app/src/main/java/com/azhar/pemesanantiket/database/AppDatabase.kt
@@ -0,0 +1,15 @@
+package com.azhar.pemesanantiket.database
+
+import androidx.room.Database
+import com.azhar.pemesanantiket.model.ModelDatabase
+import androidx.room.RoomDatabase
+import com.azhar.pemesanantiket.database.dao.DatabaseDao
+
+/**
+ * Created by Alvi (20190801245)
+ */
+
+@Database(entities = [ModelDatabase::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun databaseDao(): DatabaseDao?
+}
